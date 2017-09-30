72d1af9e77bc7b9b8889fa599351f2ba0f4abd63
Ali Darvishi <alidarvishi@Alis-MacBook-Pro.local>
2017-09-30 13:41:06 -0700

    initial structure

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyBH-OgVkuMyOaep8meq0FJD1RxavuIlXlY",
+		authDomain: "wagtive.firebaseapp.com",
+		databaseURL: "https://wagtive.firebaseio.com",
+		projectId: "wagtive",
+		storageBucket: "wagtive.appspot.com",
+		messagingSenderId: "248580578313"
+	};
+	firebase.initializeApp(config);
+
+	const db = firebase.database();
+
+})
