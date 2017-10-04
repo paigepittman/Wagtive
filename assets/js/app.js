cd7e336dc600ff83d40df57948e8dac6067b12f7
Ashley <ashley.spellman@me.com>
2017-10-03 19:25:08 -0700

    beginning authentication

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,4 +12,78 @@ $(document).ready(function(){
 
 	const db = firebase.database();
 
+	// SIGN IN WITH EMAIL AND PASSWORD
+
+$("#signin").on("click", e => {
+	event.preventDefault();
+	const email = $("#email").val().trim();
+	const password = $("#password").val().trim();
+	const auth = firebase.auth();
+
+	const promise = auth.signInWithEmailAndPassword(email, password);
+	promise.catch(e => console.log(e.message));
+})
+
+// REGISTER NEW USER WITH EMAIL AND PASSWORD
+
+$("#register").on("click", e=> {
+	event.preventDefault();
+	const email = $("#email").val().trim();
+	const password = $("#password").val().trim();
+	const auth = firebase.auth();
+	const userName = $("#name").val().trim();
+
+	const promise = auth.createUserWithEmailAndPassword(email, password).then(function(user){
+
+		// STORES ADDITIONAL DATA FROM REGISTRATION FORM
+
+		db.ref('users/' + user.uid).set({
+			firstName: userName,
+			email: email,
+			password: password
+		})
+
+		// SEND VERIFICATION EMAIL
+
+		firebase.auth().onAuthStateChanged(user => {
+		user.sendEmailVerification();
+		console.log("Verification email sent")
+	})
+
+});
+	promise.catch(e => console.log(e.message));	
+});
+
+// LOGOUT
+
+$("#logout").on("click", e=> {
+	firebase.auth().signOut();
+})
+
+
+firebase.auth().onAuthStateChanged(user => {
+	// CHECK IF USER IS SIGNED IN
+	if(user){
+		// CHECK IF SIGNED IN USERS EMAIL IS VERIFIED
+		if (user.emailVerified) {
+			var uid = user.uid;
+
+			db.ref('users/'+ uid).on('value', snapshot => {
+				activeUser = snapshot.val().firstName;
+				email = snapshot.val().email;
+				password = snapshot.val().password;
+				console.log(activeUser);
+				console.log(email);
+				console.log(password);
+
+			});
+		}
+
+	} else {
+		console.log("not logged in");
+	}
+})
+
+
+
 })
