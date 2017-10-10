5d184d740d7b588ba6660c7bb85421ced33cd8dd
BakerShoeMaker <info@quackenworth.com>
2017-10-09 18:24:46 -0700

    fixed access to objects issue.

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyBH-OgVkuMyOaep8meq0FJD1RxavuIlXlY",
@@ -10,41 +11,44 @@ $(document).ready(function() {
     };
 
     firebase.initializeApp(config);
-    const db = firebase.database();
-    const auth = firebase.auth();
 
-    firebase.auth().onAuthStateChanged(user => {
-
-    var user = firebase.auth().currentUser;
-
-    //logs current user
-    console.log("Current user: "+user.firstName);
-
-    //logs specific value of current usser
-    console.log("Current email: " +user.email);
-    console.log("Current user ID: " +user.uid);
-
-    db.ref('users/').on('value', snapshot => {
-        allUsers = snapshot.val();
-        //logs all users
-        console.log("Here are all users: " +allUsers);
-
-        //logs specific value of all users
-        var userList = []
-
-        for (var x in allUsers)
+        firebase.auth().onAuthStateChanged(user =>
         {
-            db.ref('users/' + x).on('value', snapshot =>
-                {
-                    console.log(snapshot.val())});
-                    console.log(snapshot.val().level);
-                    console.log(snapshot.child("level").val());
-                    //var firstName = snapshot.child("name/first").val();
-                }
+             db = firebase.database();
+             user = firebase.auth().currentUser;
+
+             //logs current user
+             console.log(user)
+
+                //logs specific value of current usser
+                console.log(user.email)
+
+                db.ref('users/').on('value', snapshot => {
+                allUsers = snapshot.val();
+
+                //logs all users
+                console.log(allUsers)
+
+                //logs specific value of all users
+                var userList = []
+
+                for (var x in allUsers) {
+
+                    db.ref('users/' + x).on('value', snapshot => {
+                     //console.log(snapshot.val());
+                    console.log("The level is: " +snapshot.val().level);
+                    console.log("The first name is: " +snapshot.val().firstName);
+                })
+
+            }
 
         })
 
     })
 
-});
+})
 
+    ////console.log(snapshot.val());
+    //console.log(snapshot.val().level);
+    //console.log(snapshot.child("level").val());
+//var firstName = snapshot.child("name/first").val();
